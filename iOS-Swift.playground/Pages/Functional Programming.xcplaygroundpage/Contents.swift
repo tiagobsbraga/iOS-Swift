279670df0a8f50cf9927e6a7c4bb4630dcd2c695
Txai Wieser <txaidw@gmail.com>
2015-10-15 12:48:59 -0300

    Added Filter, Reduce and Map to the Functional Programming page.

--- iOS-Swift.playground/Pages/Functional Programming.xcplaygroundpage/Contents.swift
+++ iOS-Swift.playground/Pages/Functional Programming.xcplaygroundpage/Contents.swift
@@ -3,3 +3,32 @@
 //: [Previous](@previous)
 
 import Foundation
+
+//: Filtering
+
+let completeArray = Array(0..<10)
+
+let evenNumbers = completeArray.filter { $0 % 2 == 0 }
+
+print(evenNumbers)
+
+
+//: Reducing
+
+let purchases = [0.35, 4.82, 8.34, 8.0, 2.4, 5.6, 9.9, 6.3, 4.6, 7.7]
+
+let total = purchases.reduce(0) { $0 + $1 }
+
+print(total)
+
+
+//: Mapping
+
+let numbers = Array(0..<10)
+
+let strings = numbers.map { "The number is " + String($0) }
+
+print(strings)
+
+
+//: [Next](@next)
